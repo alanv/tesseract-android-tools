625fa9335276832565961360b8ad0a75597f166d
Alan Viverette <alanv@google.com>
2012-01-14 00:39:15 -0800

    Updated to compile latest versions of Leptonica and Tesseract.

--- tesseract-android-tools/jni/Application.mk
+++ tesseract-android-tools/jni/Application.mk
@@ -1,5 +1,4 @@
-# ARMv7 is significanly faster due to the use of the hardware FPU
 APP_STL := gnustl_static
-APP_ABI := armeabi armeabi-v7a
+APP_ABI := armeabi armeabi-v7a x86
 APP_OPTIM := release
 APP_CPPFLAGS += -fexceptions -frtti
