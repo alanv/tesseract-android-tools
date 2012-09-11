d0029e332405ae2f3d4d4c66b8c80f2a33b351fb
Alan Viverette <alanv@alanv-macbookpro.local>
2012-09-11 00:29:39 -0700

    Changing target to API 8, since we require native bitmap support.

--- tesseract-android-tools/jni/Application.mk
+++ tesseract-android-tools/jni/Application.mk
@@ -1,5 +1,4 @@
 APP_STL := gnustl_static
 APP_ABI := armeabi armeabi-v7a x86 # mips has problems with gnustl_static
 APP_OPTIM := release
-APP_PLATFORM := android-8
 APP_CPPFLAGS += -fexceptions -frtti
